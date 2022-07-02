db2394b2675a03ba7935cfd2856566c7d3b74bfc
HighPon <s.shiraki081@gmail.com>
2022-07-02 18:02:37 +0900

    plotのタイトル、ラベル、フォントを初期化する処理を追加

--- lecture/common.go
+++ lecture/common.go
@@ -1,12 +1,23 @@
 package lecture
 
 import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"io"
 	"io/fs"
+	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/pkg/errors"
+	"golang.org/x/image/font/opentype"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/font"
+	"gonum.org/v1/plot/plotter"
 )
 
 func CheckExistDir(path string) error {
@@ -35,4 +46,79 @@ func GetFileLists(inputPath string) ([]string, error) {
 		return nil
 	})
 	return findList, err
+}
+
+func PlotInit(title, xLabel, yLabel string) *plot.Plot {
+	p := plot.New()
+
+	p.Title.Text = title
+	p.X.Label.Text = xLabel
+	p.Y.Label.Text = yLabel
+
+	p.Add(plotter.NewGrid())
+
+	return p
+}
+
+func PlotFontInit() {
+	// download font from debian
+	const url = "http://http.debian.net/debian/pool/main/f/fonts-ipafont/fonts-ipafont_00303.orig.tar.gz"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalf("could not download IPA font file: %+v", err)
+	}
+	defer resp.Body.Close()
+
+	ttf, err := untargz("IPAfont00303/ipam.ttf", resp.Body)
+	if err != nil {
+		log.Fatalf("could not untar archive: %+v", err)
+	}
+
+	fontTTF, err := opentype.Parse(ttf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	mincho := font.Font{Typeface: "Mincho"}
+	font.DefaultCache.Add([]font.Face{
+		{
+			Font: mincho,
+			Face: fontTTF,
+		},
+	})
+	if !font.DefaultCache.Has(mincho) {
+		log.Fatalf("no font %q!", mincho.Typeface)
+	}
+	plot.DefaultFont = mincho
+	plotter.DefaultFont = mincho
+}
+
+func untargz(name string, r io.Reader) ([]byte, error) {
+	gr, err := gzip.NewReader(r)
+	if err != nil {
+		return nil, fmt.Errorf("could not create gzip reader: %v", err)
+	}
+	defer gr.Close()
+
+	tr := tar.NewReader(gr)
+	for {
+		hdr, err := tr.Next()
+		if err != nil {
+			if err == io.EOF {
+				return nil, fmt.Errorf("could not find %q in tar archive", name)
+			}
+			return nil, fmt.Errorf("could not extract header from tar archive: %v", err)
+		}
+
+		if hdr == nil || hdr.Name != name {
+			continue
+		}
+
+		buf := new(bytes.Buffer)
+		_, err = io.Copy(buf, tr)
+		if err != nil {
+			return nil, fmt.Errorf("could not extract %q file from tar archive: %v", name, err)
+		}
+		return buf.Bytes(), nil
+	}
 }
